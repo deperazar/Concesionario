3aace9add3dadf8e3228f0f752cc7ce22bea0e89
Estudiante <Estudiante@10.203.1.126>
2017-03-21 16:02:17 -0500

    hotel

--- JavaApplication17/src/javaapplication17/Habitacion.java
+++ JavaApplication17/src/javaapplication17/Habitacion.java
@@ -6,15 +6,22 @@ package javaapplication17;
 public class Habitacion {
     private int capacidadmaxima;
     private String IDhabitacion;
-   
+    private Precio price;
+    private Persona[] p;
     public Habitacion(int cp, String IDhab){
         capacidadmaxima=cp;
         IDhabitacion=IDhab;
+        p = new Persona[cp];
      
     }
     
     
     public void setIDhab(int j,int i){
         this.IDhabitacion=j+"0"+i;
+    }
+    
+    public void addpersona(Persona i){
+        
+        p[1]= i;
     }
 }
